cd0ec3554c4c8b1e328da0f7e30355ab013154a0
egwkim <35051914+egwkim@users.noreply.github.com>
2022-03-21 23:22:38 +0900

    Update main.ts
    Update DrawingApp class.
     - Creates boxes on mouse drag.
     - Changes the colors of the boxes over time.
    
    Add color2RGBString function and HSVtoRGB function.

--- main.ts
+++ main.ts
@@ -1,8 +1,17 @@
 class DrawingApp {
+  private static boxCreationInterval = 100;
+  private static colorUpdateStep = 0.005;
+
   private canvas: HTMLCanvasElement;
   private context: CanvasRenderingContext2D;
 
+  private mouseDown: boolean;
+
+  private prevBoxTimestamp: number;
+
   private boxes: Box[];
+
+  private color: number;
 
   constructor() {
     let canvas = document.getElementById('canvas') as HTMLCanvasElement;
@@ -11,7 +20,13 @@ class DrawingApp {
     this.canvas = canvas;
     this.context = context;
 
+    this.mouseDown = false;
+
+    this.prevBoxTimestamp = 0;
+
     this.boxes = [];
+
+    this.color = 0;
 
     this.addListeners();
     this.resizeCanvas();
@@ -24,7 +39,20 @@ class DrawingApp {
     window.addEventListener('resize', this.resizeCanvas);
 
     canvas.addEventListener('mousedown', (e) => {
+      this.mouseDown = true;
       this.createBox(e.x, e.y);
+    });
+
+    canvas.addEventListener('mousemove', (e) => {
+      let now = performance.now();
+      if (this.mouseDown && now - this.prevBoxTimestamp > DrawingApp.boxCreationInterval) {
+        this.createBox(e.x, e.y);
+        this.prevBoxTimestamp = now;
+      }
+    });
+
+    canvas.addEventListener('mouseup', (e) => {
+      this.mouseDown = false;
     });
   }
 
@@ -37,8 +65,14 @@ class DrawingApp {
     this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
   }
 
-  private createBox(x: number, y: number) {
-    this.boxes.push(new Box(x, y));
+  private createBox(x: number, y: number, updateColor: boolean = true, color?: number) {
+    color ? this.boxes.push(new Box(x, y, color)) : this.boxes.push(new Box(x, y, this.color));
+    if (updateColor) {
+      this.color += DrawingApp.colorUpdateStep;
+      if (this.color === 1) {
+        this.color = 0;
+      }
+    }
   }
 
   private Render = () => {
@@ -73,12 +107,14 @@ class Box {
   private y: number;
   private size: number;
   private tick: number;
+  private color: number;
 
-  constructor(x: number, y: number) {
+  constructor(x: number, y: number, color: number) {
     this.x = x;
     this.y = y;
     this.size = 0;
     this.tick = 0;
+    this.color = color;
   }
 
   public update() {
@@ -93,9 +129,49 @@ class Box {
   }
 
   public draw(context: CanvasRenderingContext2D) {
+    context.fillStyle = color2RGBString(this.color);
     context.globalAlpha = 1 - this.tick / 200;
     context.fillRect(this.x, this.y, this.size, this.size);
   }
 }
 
 new DrawingApp();
+
+function color2RGBString(color: number): string {
+  const rgb = HSVtoRGB(color, 1, 1);
+  return `rgb(${rgb.r}, ${rgb.g}, ${rgb.b})`;
+}
+
+function HSVtoRGB(h: number, s: number, v: number) {
+  let r: number, g: number, b: number, i: number, f: number, p: number, q: number, t: number;
+  i = Math.floor(h * 6);
+  f = h * 6 - i;
+  p = v * (1 - s);
+  q = v * (1 - f * s);
+  t = v * (1 - (1 - f) * s);
+  switch (i % 6) {
+    case 0:
+      (r = v), (g = t), (b = p);
+      break;
+    case 1:
+      (r = q), (g = v), (b = p);
+      break;
+    case 2:
+      (r = p), (g = v), (b = t);
+      break;
+    case 3:
+      (r = p), (g = q), (b = v);
+      break;
+    case 4:
+      (r = t), (g = p), (b = v);
+      break;
+    case 5:
+      (r = v), (g = p), (b = q);
+      break;
+  }
+  return {
+    r: Math.round(r * 255),
+    g: Math.round(g * 255),
+    b: Math.round(b * 255),
+  };
+}
