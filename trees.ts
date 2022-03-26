2f6e1f4327de403e250a8460a3924b4e930d2c19
egwkim <35051914+egwkim@users.noreply.github.com>
2022-03-26 21:57:10 +0900

    Update Seed render method
    Updated Seed render method.
     - Generates seeds with random colors.
     - Added seed trails.
    
    Added treeUpdateTick in TreeCanvasApp.

--- trees.ts
+++ trees.ts
@@ -1,9 +1,13 @@
-import { CanvasApp, Vector2D } from './main.js';
+import { CanvasApp, Vector2D, hue2RGBString } from './main.js';
 import { LSystem } from './l-system.js';
 
 class TreeCanvasApp extends CanvasApp {
+  static treeUpdateTick = 30;
+
   private trees: Tree[];
   private seeds: Seed[];
+
+  private tick: number;
 
   constructor() {
     super();
@@ -11,8 +15,10 @@ class TreeCanvasApp extends CanvasApp {
 
   protected init(): void {
     this.trees = [];
-    this.trees.push(new Tree(40, 40)); // Temporary
     this.seeds = [];
+    this.tick = 0;
+
+    this.trees.push(new Tree(40, 40)); // Temporary
     this.seeds.push(new Seed(50, 50)); // Temporary
   }
 
@@ -34,15 +40,19 @@ class TreeCanvasApp extends CanvasApp {
       index -= 1;
     }
 
-    index = this.trees.length - 1;
-    while (index !== -1) {
-      if (!this.trees[index].alive) {
-        this.trees.splice(index, 1);
-      } else {
-        this.trees[index].update();
+    if (this.tick % TreeCanvasApp.treeUpdateTick == 0) {
+      index = this.trees.length - 1;
+      while (index !== -1) {
+        if (!this.trees[index].alive) {
+          this.trees.splice(index, 1);
+        } else {
+          this.trees[index].update();
+        }
+        index -= 1;
       }
-      index -= 1;
     }
+
+    this.tick += 1;
   }
 
   protected render(): void {
@@ -54,7 +64,7 @@ class TreeCanvasApp extends CanvasApp {
 }
 
 class Seed {
-  static acceleration = new Vector2D(0, 0.1);
+  static acceleration = new Vector2D(0, 0.2);
 
   public pos: Vector2D;
   public prev_pos: Vector2D;
@@ -62,6 +72,8 @@ class Seed {
   private velocity: Vector2D;
 
   private destinateY: number;
+
+  private color: string;
 
   get alive() {
     return this.pos.y < this.destinateY;
@@ -72,6 +84,8 @@ class Seed {
     this.velocity = new Vector2D(0, 0);
     this.prev_pos = new Vector2D(x, y);
     this.destinateY = 400; // Temporary value
+
+    this.color = hue2RGBString(Math.random());
   }
 
   public update(): void {
@@ -81,14 +95,30 @@ class Seed {
     this.velocity.add(Seed.acceleration);
   }
 
-  // TODO Change the appearance of the seed object.
+  // FIXME Low visibility of dark blue seeds.
   public render(context: CanvasRenderingContext2D): void {
     context.beginPath();
-    context.moveTo(this.pos.x, this.pos.y);
-    context.lineTo(this.pos.x, this.pos.y - this.velocity.y * 20);
-    context.strokeStyle = 'white';
-    context.lineWidth = 10;
+
+    let x0 = this.pos.x,
+      x1 = this.pos.x,
+      y0 = this.pos.y,
+      y1 = this.pos.y - this.velocity.y * 10;
+
+    let grad = context.createLinearGradient(x0, y0, x1, y1);
+
+    grad.addColorStop(0, this.color);
+    grad.addColorStop(1, 'black');
+
+    context.moveTo(x0, y0);
+    context.lineTo(x1, y1);
+    context.strokeStyle = grad;
+    context.lineWidth = 5;
     context.stroke();
+
+    context.beginPath();
+    context.arc(this.pos.x, this.pos.y, 5, 0, Math.PI * 2);
+    context.fillStyle = this.color;
+    context.fill();
   }
 }
 
@@ -119,6 +149,7 @@ class Tree extends LSystem {
   public update() {
     super.update();
     this.life -= 1;
+    console.log(this.state);
   }
 
   // TODO Tree render method
